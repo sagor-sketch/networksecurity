827c7a4b62492c4bf7ecab1308700c34819c261d
Sagor <sagarsaha455@gmail.com>
2026-01-21 22:20:06 +0100

    added mlflow

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -66,7 +66,7 @@ class ModelTrainer:
                 # There are other ways to use the Model Registry, which depends on the use case,
                 # please refer to the doc for more information:
                 # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.sklearn.log_model(best_model, "model", registered_model_name=best_model)
+                mlflow.sklearn.log_model(sk_model = best_model,artifact_path="model")
             else:
                 mlflow.sklearn.log_model(best_model, "model")
 
